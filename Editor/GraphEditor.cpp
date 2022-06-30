b89a8a1e73bf9e442b8faa2cb939e3f6ba4a6ce0
AKGWSB <252009914@qq.com>
2022-07-01 04:05:33 +0800

    remake graph editor

--- Editor/GraphEditor.cpp
+++ Editor/GraphEditor.cpp
@@ -1,4 +1,5 @@
 #include "GraphEditor.h"
+#include "ResourceViewer.h"
 
 #include "../Library/imgui/imgui.h"
 #include "../Library/imgui/imgui_impl_win32.h"
@@ -7,8 +8,143 @@
 
 #include <random>
 
-std::set<int> GraphEditor::usedNodeID;
-std::set<int> GraphEditor::usedPinID;
+// ----------------------------------------------------------------------- //
+
+Pin::Pin()
+{
+
+}
+
+Pin::Pin(PinType t, std::string n) : 
+    type(t), 
+    name(n)
+{
+
+}
+
+// ----------------------------------------------------------------------- //
+
+Node::Node()
+{
+
+}
+
+void Node::RenderPins()
+{
+    // in pins
+    for (auto& id : inputPins)
+    {
+        Pin& ipin = GraphEditor::GetPinByID(id);
+        ImNodes::BeginInputAttribute(ipin.runtimeID);
+        ImGui::Text(ipin.name.c_str());
+        ImNodes::EndInputAttribute();
+    }
+
+    // out pins
+    for (auto& id : outputPins)
+    {
+        Pin& opin = GraphEditor::GetPinByID(id);
+        ImNodes::BeginOutputAttribute(opin.runtimeID);
+        ImGui::Text(opin.name.c_str());
+        ImNodes::EndOutputAttribute();
+    }
+}
+
+void Node::Render()
+{
+    ImNodes::BeginNode(runtimeID);
+
+    if (type == NodeType::RenderPassNode)
+    {
+        ImNodes::BeginNodeTitleBar();
+        ImGui::TextUnformatted(name.c_str());
+        ImNodes::EndNodeTitleBar();
+
+        RenderPins();
+    }
+
+    if (type == NodeType::BlitPassNode)
+    {
+        ImNodes::BeginNodeTitleBar();
+        ImGui::TextUnformatted(name.c_str());
+        ImNodes::EndNodeTitleBar();
+
+        RenderPins();
+    }
+
+    if (type == NodeType::RenderTextureNode)
+    {
+        ImNodes::BeginNodeTitleBar();
+        ImGui::TextUnformatted(name.c_str());
+        ImNodes::EndNodeTitleBar();
+
+        RenderPins();
+
+        // convert to char* array
+        std::vector<std::string> renderTextureList = RenderTexture::GetNameList();
+        renderTextureList.insert(renderTextureList.begin(), "None");
+        int n_rt = renderTextureList.size();
+        std::vector<const char*> chars;
+        for (auto& s : renderTextureList)
+        {
+            chars.push_back(s.c_str());
+        }
+        // draw select box
+        static int rtNodeSelectIndex = 0;
+        ImGui::PushItemWidth(128.0f);
+        ImGui::Combo("##rt_combo", &rtNodeSelectIndex, chars.data(), n_rt);
+        ImGui::PopItemWidth();
+        resourceName = renderTextureList[rtNodeSelectIndex];
+
+        // not select
+        if (resourceName != "None")
+        {
+            Texture2D* tex = RenderTexture::Find(resourceName);
+            auto hdptr = D3D12_GPU_DESCRIPTOR_HANDLE(tex->srvGpuHandle).ptr;
+            ImGui::Image((ImTextureID)hdptr, ImVec2(128, 128), ImVec2(0, 0), ImVec2(1, 1));
+        }
+
+        ImGui::Dummy(ImVec2(0.0f, 10.0f));
+    }
+
+    if (type == NodeType::Texture2DNode)
+    {
+        ImNodes::BeginNodeTitleBar();
+        ImGui::TextUnformatted(name.c_str());
+        ImNodes::EndNodeTitleBar();
+
+        RenderPins();
+
+        // not select
+        if (resourceName.length() != 0)
+        {
+            Texture2D* tex = Texture2D::Find(resourceName);
+            auto hdptr = D3D12_GPU_DESCRIPTOR_HANDLE(tex->srvGpuHandle).ptr;
+            ImGui::Image((ImTextureID)hdptr, ImVec2(128, 128), ImVec2(0, 1), ImVec2(1, 0));
+        }
+
+        // button
+        std::string buttonName = "select texture";
+        std::string buttonID = buttonName + std::to_string(runtimeID); // identifier
+        if (ImGui::Button(buttonName.c_str()))
+        {
+            ResourceViewer::Open(buttonID.c_str());
+        }
+        ResourceViewer::GetSelectResourceName(resourceName, buttonID);
+    }
+
+    if (type == NodeType::ScreenTargetNode)
+    {
+
+    }
+
+    ImNodes::EndNode();
+}
+
+// ----------------------------------------------------------------------- //
+
+std::map<int, Node> GraphEditor::nodePool;
+std::map<int, Pin> GraphEditor::pinPool;
 
 GraphEditor::GraphEditor()
 {
@@ -22,49 +158,63 @@ GraphEditor::~GraphEditor()
 
 void GraphEditor::Init()
 {
-    renderPassNodes = { RegisterNewNode<RenderPassNode>("BasePass") };
-    /**/
-    blitPassNodes = { 
-        RegisterNewNode<BlitPassNode>("lightpass"), 
-        RegisterNewNode<BlitPassNode>("finalPass") 
-    };
-    textureNodes = { RegisterNewNode<TextureNode>("Tex_0") };
-    renderTextureNodes = { RegisterNewNode<RenderTextureNode>("RT_0") };
+    Node basePassNode;
+    basePassNode.type = NodeType::RenderPassNode;
+    basePassNode.name = "basePass";
+    basePassNode.outputPins = { GraphEditor::RegisterNewPin(Pin(PinType::RenderTargetOutput, "RT_0")) };
+    GraphEditor::RegisterNewNode(basePassNode);
+
+    Node blitPassNode;
+    blitPassNode.type = NodeType::BlitPassNode;
+    blitPassNode.name = "blitPass";
+    blitPassNode.inputPins = { GraphEditor::RegisterNewPin(Pin(PinType::RenderTargetOutput, "MainTex_0")) };
+    blitPassNode.outputPins = { GraphEditor::RegisterNewPin(Pin(PinType::RenderTargetOutput, "RT_0")) };
+    GraphEditor::RegisterNewNode(blitPassNode);
+
+    Node rtNode;
+    rtNode.type = NodeType::RenderTextureNode;
+    rtNode.name = "RT_BasePass";
+    rtNode.inputPins = { GraphEditor::RegisterNewPin(Pin(PinType::TextureWrite, "write")) };
+    rtNode.outputPins = { GraphEditor::RegisterNewPin(Pin(PinType::TextureRead, "read")) };
+    GraphEditor::RegisterNewNode(rtNode);
+
+    Node texNode;
+    texNode.type = NodeType::Texture2DNode;
+    texNode.name = "Tex_0";
+    texNode.outputPins = { GraphEditor::RegisterNewPin(Pin(PinType::TextureRead, "read")) };
+    GraphEditor::RegisterNewNode(texNode);
 }
 
 void GraphEditor::RenderUI()
 {
+    ImGui::SetNextWindowBgAlpha(0.35f);
     ImGui::Begin("Pipeline editor");
+
+    if (ImGui::Button("Save Pipeline"))
+    {
+        SaveToFile("Asset/e_pipeline.pipeline");
+    }
+
     ImNodes::BeginNodeEditor();
     
     // render nodes
-    for(auto& node : renderPassNodes)
+    for(auto& p : nodePool)
     {
-        node.Render();
-    }
-    for (auto& node : blitPassNodes)
-    {
-        node.Render();
-    }
-    for (auto& node : textureNodes)
-    {
-        node.Render();
-    }
-    for (auto& node : renderTextureNodes)
-    {
+        Node& node = p.second;
         node.Render();
     }
 
+    /**/
     // render links
     ImNodes::PushColorStyle(ImNodesCol_Link, IM_COL32(11, 109, 191, 255));
     ImNodes::PushColorStyle(ImNodesCol_LinkHovered, IM_COL32(255, 255, 128, 255));
     ImNodes::PushColorStyle(ImNodesCol_LinkSelected, IM_COL32(81, 148, 204, 255));
-    for (int i = 0; i < edges.size(); ++i)
+    for (int i = 0; i < links.size(); ++i)
     {
-        const std::pair<int, int> p = edges[i];
+        Link& link = links[i];
 
-        int start_pin_id = p.first;
-        int end_pin_id = p.second;
+        int start_pin_id = link.srcPin;
+        int end_pin_id = link.dstPin;
         int link_id = i;
 
         ImNodes::Link(link_id, start_pin_id, end_pin_id);
@@ -76,11 +226,15 @@ void GraphEditor::RenderUI()
     ImNodes::EndNodeEditor();
     ImGui::End();
 
+    /**/
     //create links
     int start_pin_id, end_pin_id;
     if (ImNodes::IsLinkCreated(&start_pin_id, &end_pin_id))
     {
-        edges.push_back(std::make_pair(start_pin_id, end_pin_id));
+        Link newLink;
+        newLink.srcPin = start_pin_id;
+        newLink.dstPin = end_pin_id;
+        links.push_back(newLink);
     }
 
     // delete links
@@ -91,53 +245,64 @@ void GraphEditor::RenderUI()
         
         if (ImGui::IsKeyPressed(ImGuiKey_::ImGuiKey_Delete))
         {
-            edges.erase(edges.begin() + hovered_link_id);
+            links.erase(links.begin() + hovered_link_id);
         }
     }
 }
 
-
-template <typename T_NODE>
-T_NODE GraphEditor::RegisterNewNode(std::string nodeName)
+int GraphEditor::RegisterNewNode(const Node& node)
 {
-    // node id
     int id;
     do
     {
         id = rand();
-    } while (usedNodeID.find(id) != usedNodeID.end());
-    usedNodeID.insert(id);
+    } while (nodePool.find(id) != nodePool.end());
 
-    T_NODE newNode;
-    newNode.id = id;
-    newNode.name = nodeName;
+    nodePool[id] = node;
+    nodePool[id].runtimeID = id;
 
-    // register pin id for all pins
-    for (auto& pin : newNode.inputPins)
-    {
-        pin = RegisterNewPin(pin.name);
-    }
-    for (auto& pin : newNode.outputPins)
-    {
-        pin = RegisterNewPin(pin.name);
-    }
-
-    return newNode;
+    return id;
 }
 
-Pin GraphEditor::RegisterNewPin(std::string pinName)
+int GraphEditor::RegisterNewPin(const Pin& pin)
 {
     int id;
     do
     {
         id = rand();
-    } while (usedPinID.find(id) != usedPinID.end());
-    usedPinID.insert(id);
+    } while (pinPool.find(id) != pinPool.end());
 
-    Pin newPin;
-    newPin.id = id;
-    newPin.name = pinName;
+    pinPool[id] = pin;
+    pinPool[id].runtimeID = id;
 
-    return newPin;
+    return id;
+}
+
+Node& GraphEditor::GetNodeByID(int runtimeID)
+{
+    if (nodePool.find(runtimeID) == nodePool.end())
+    {
+        throw std::exception();
+    }
+    return nodePool[runtimeID];
+}
+
+Pin& GraphEditor::GetPinByID(int runtimeID)
+{
+    if (pinPool.find(runtimeID) == pinPool.end())
+    {
+        throw std::exception();
+    }
+    return pinPool[runtimeID];
+}
+
+void GraphEditor::SaveToFile(std::string filepath)
+{
+    
+}
+
+void GraphEditor::LoadFromFile(std::string filepath)
+{
+
 }
 
--- Editor/GraphEditor.h
+++ Editor/GraphEditor.h
@@ -5,32 +5,104 @@
 #include <string>
 #include <vector>
 
-#include "GraphicNode.h"
+// ----------------------------------------------------------------------- //
+
+enum PinType
+{
+	TextureRead,
+	TextureWrite,
+	RenderTargetOutput
+};
+
+class Pin
+{
+public:
+	// load from file
+	PinType type;
+	std::string name;
+
+	// Pin's id, register while runtime
+	int runtimeID;		
+
+	Pin();
+	Pin(PinType t, std::string n);
+};
+
+// ----------------------------------------------------------------------- //
+
+enum NodeType
+{
+	RenderPassNode,
+	BlitPassNode,
+	Texture2DNode,
+	RenderTextureNode,
+	ScreenTargetNode
+};
+
+class Node
+{
+public:
+	NodeType type;
+
+	// the resource held by node
+	// maybe a Texture2D, RenderTexture or ...
+	std::string name;
+	std::string resourceName;
+
+	// list of io pins
+	// storage pin's runtimeID
+	std::vector<int> inputPins;		// find pin by Link.srcPin
+	std::vector<int> outputPins;	// find pin by Link.dstPin
+
+	// node's id, register while runtime
+	int runtimeID;
+
+	Node();
+
+	void RenderPins();
+
+	// render depends on node's type
+	void Render();
+};
+
+// ----------------------------------------------------------------------- //
+
+// a link from source node's output pin to destination node's input pin
+class Link
+{
+public:
+	int srcNode;
+	int dstNode;
+	int srcPin;
+	int dstPin;
+
+	// link's id, register while runtime
+	int runtimeID;
+};
+
+// ----------------------------------------------------------------------- //
 
 class GraphEditor
 {
 private:
-	static std::set<int> usedNodeID;
-	static std::set<int> usedPinID;
+	// find by runtimeID
+	static std::map<int, Node> nodePool;
+	static std::map<int, Pin> pinPool;
 
-	// pin id : (start, end)
-	std::vector<std::pair<int, int>> edges;
-	
-	std::vector<RenderPassNode> renderPassNodes;
-	std::vector<BlitPassNode> blitPassNodes;
-	std::vector<TextureNode> textureNodes;
-	std::vector<RenderTextureNode> renderTextureNodes;
+	std::vector<Link> links;
 
 public:
 	GraphEditor();
 	~GraphEditor();
 
-	template <typename T_NODE>
-	static T_NODE RegisterNewNode(std::string nodeName);
-
-	static Pin RegisterNewPin(std::string pinName);
+	static int RegisterNewNode(const Node& node);
+	static int RegisterNewPin(const Pin& pin);
+	static Node& GetNodeByID(int runtimeID);
+	static Pin& GetPinByID(int runtimeID);
 
 	void Init();
 	void RenderUI();
+	void SaveToFile(std::string filepath);
+	void LoadFromFile(std::string filepath);
 };
 
